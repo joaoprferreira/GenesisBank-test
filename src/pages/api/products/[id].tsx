ef2382d861a13b4df29c002938ca8b63cfe4abbd
joão Pedro <joaof082@gmail.com>
2023-05-27 02:25:13 -0300

    feat: Building functions that  make new values in table and removing and edit
    Signed-off-by: joão Pedro <joaof082@gmail.com>

---
+++ src/pages/api/products/[id].tsx
@@ -0,0 +1,76 @@
+import { dbConnection } from '../../../config/db'
+import { useRouter } from 'next/router'
+
+export async function query(query: any, values?: any) {
+  try {
+    const [results] = await dbConnection.execute(query, values);
+    dbConnection.end();
+    return results;
+  } catch (error: any) {
+    throw Error(error.message);
+    return { error };
+  }
+}
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      return await getProductSpecific(req, res);
+    case "DELETE":
+      return await deleteProduct(req, res);
+    case "PUT":
+      return await UpdateProduct(req, res);
+    default:
+      return res.status(400).json({ message: "bad request" });
+  }
+}
+
+async function getProductSpecific(req, res) {
+  try {
+    const result = await dbConnection.query('SELECT * FROM products WHERE id = ?', [
+      req.query.id
+    ])
+    return res.status(200).json(result[0])
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+async function UpdateProduct(req, res) {
+
+  const { query: QueryParam } = useRouter()
+  try {
+
+    const productName = req.body.name;
+    const productDescription = req.body.description;
+    const productPrice = req.body.preice;
+    const productImage = req.body.image
+
+    const body = {
+      name: productName,
+      description: productDescription,
+      price: productPrice,
+      image: productImage
+    }
+
+    console.log("REQ::", req)
+
+    await query(
+      `UPDATE products SET ? WHERE id = ? `,
+      [req.body, req.query.id]
+    );
+
+    return res.status(204).json();
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+async function deleteProduct(req, res) {
+  try {
+    await dbConnection.query('DELETE FROM products WHERE id = ?', [req.query.id])
+    return res.status(204).json()
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message })
+  }
+}
