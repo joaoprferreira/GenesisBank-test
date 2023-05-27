ab560aae53e8de6c2225c42272cbccd2529d6110
joão Pedro <joaof082@gmail.com>
2023-05-27 09:48:10 -0300

    feat: adding colorModeScript of chackra ui in document
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
+import { theme } from '@/commons/styles/theme'
 
 export default function Document() {
   return (
@@ -8,6 +10,7 @@ export default function Document() {
         <title> e-commerce </title>
       </Head>
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
