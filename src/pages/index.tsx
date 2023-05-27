0a8fb6526adfc7e60adbbb5a50567352ad7642ba
joão Pedro <joaof082@gmail.com>
2023-05-27 09:48:35 -0300

    feat: Products in Home
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { userStore } from "@/store/products"
+import { Link } from "@chakra-ui/next-js"
+import Products from "./products"
 
 export default function Home() {
 
   return (
     <>
+      <Products />
+      <Link href='/about' color='blue.100' _hover={{ color: 'blue.500' }}>
+        About
+      </Link>
       <h1>Hello word</h1>
     </>
   )
