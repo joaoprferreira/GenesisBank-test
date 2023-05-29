d3e4a19ec0ec535bc1b7b46642619ac09746c90b
joão Pedro <joaof082@gmail.com>
2023-05-29 20:02:56 -0300

    feat: typing request and response
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,10 @@
 import { query } from "@/config/db";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
     const products = await query({
       query: "SELECT * FROM products",
