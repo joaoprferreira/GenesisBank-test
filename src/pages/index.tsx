fbc7647631e5e8e93f9e80407d8190ae36bb68b6
joão Pedro <joaof082@gmail.com>
2023-05-29 20:15:39 -0300

    refactor: removing components of link and other that no was used
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
-import { userStore } from "@/store/products"
-import { Link } from "@chakra-ui/next-js"
+import { Providers } from "./Layout"
 import Products from "./products"
 
 export default function Home() {
 
   return (
-    <>
+    <Providers>
       <Products />
-      <Link href='/about' color='blue.100' _hover={{ color: 'blue.500' }}>
-        About
-      </Link>
-      <h1>Hello word</h1>
-    </>
+    </Providers>
   )
 }
