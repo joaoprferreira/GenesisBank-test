7733da3af6914985b56a724be8a523aaf375d369
joão Pedro <joaof082@gmail.com>
2023-05-28 21:26:55 -0300

    feat: Building table of products and shoppingCart
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/database/001-e-commerce.sql
+++ src/database/001-e-commerce.sql
@@ -1,10 +1,24 @@
 -- Active: 1685132542593@@localhost@3306@crud
 
+CREATE DATABASE ecommerce IF NOT EXISTS;
+
+use ecommerce;
+
 CREATE TABLE
-    IF NOT EXISTS PRODUCTS (
-        id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
         name VARCHAR(100) NOT NULL,
         description VARCHAR(100) NOT NULL,
         price VARCHAR(50) NOT NULL,
         image TEXT NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS shoppingCart(
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        product_id INT,
+        quantity INTEGER,
+        total DECIMAL(10, 2),
+        FOREIGN KEY (product_id) REFERENCES products(id)
     )
+describe products
