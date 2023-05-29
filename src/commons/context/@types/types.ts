74f360d7743713211b480518370836f097059f95
joão Pedro <joaof082@gmail.com>
2023-05-29 20:08:21 -0300

    feat: Building types to products and shopping carrt
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/commons/context/@types/types.ts
+++ src/commons/context/@types/types.ts
@@ -1,5 +1,5 @@
 export interface product {
-  id: number;
+  id?: number;
   name: string;
   category: string;
   description: string;
@@ -7,21 +7,38 @@ export interface product {
   image: string;
 }
 
+export interface productCart {
+  id: number;
+  product_id: number;
+  amount: number;
+  total: string;
+  price: number;
+  produto: product;
+}
+
 export interface IProductsContext {
-  // state: product[];
   product: InitialState;
   dispatch: React.Dispatch<any>;
   addAllProducts: () => void;
-  // getItemByPage: (pageNumber: number, itemsPerPage: number) => any;
+  filteredProduct: product[];
+  productsPaginated: product[];
 }
 
 export interface IProductProviderContext {
   children: React.ReactNode;
 }
 
+export interface filterState {
+  name: string;
+  category: string;
+}
+
 export interface InitialState {
   products: product[] | [];
   currentPage: number;
   itensPaginated: product[];
-  shoppingCart: product[];
+  totalPriceProducts: number;
+  shoppingCart: productCart[];
+  isLoading: false;
+  filter: filterState;
 }
