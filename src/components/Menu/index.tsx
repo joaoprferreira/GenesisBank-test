3df0bdb5fc41c42ae52e9a5b6c52f10e1af20ac0
joão Pedro <joaof082@gmail.com>
2023-05-28 21:24:04 -0300

    feat: making test of pagination
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,18 @@
-import { Box, Icon } from "@chakra-ui/react";
+import { Box, Button, Icon } from "@chakra-ui/react";
 import { MdShoppingCart } from 'react-icons/md'
 import { CgShoppingBag, CgAdd } from 'react-icons/cg'
 import { InitialFocus } from "../ModalRegisterProduct";
+import { useContext } from "react";
+import { ProductContext } from "@/commons/context/productContext";
 
 export default function Menu() {
+  const { dispatch } = useContext(ProductContext)
+
   return (
     <Box h='90%' w='4%' mt={10} bg='white' display='flex' justifyContent='flex-start' alignItems='center' flexDirection='column' >
-      <Icon as={CgShoppingBag} w={10} h='auto' mb={50} />
+      <Button onClick={() => dispatch({ type: 'SET_NEXT_PAGE' })}>
+        <Icon as={CgShoppingBag} w={10} h='auto' mb={50} bgColor='gray' />
+      </Button>
       <InitialFocus />
     </Box >
   )
