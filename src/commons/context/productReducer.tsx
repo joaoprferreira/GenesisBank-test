7bfc224ae67182d348eb9a4af44a2c1b1d208747
joão Pedro <joaof082@gmail.com>
2023-05-29 20:06:16 -0300

    feat: Building reducer to all applications
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/commons/context/productReducer.tsx
+++ src/commons/context/productReducer.tsx
@@ -1,13 +1,6 @@
-import { useContext } from "react"
-import { InitialState, product } from "./@types/types"
-import { ProductContext } from "./productContext"
-
+import { InitialState } from "./@types/types"
 
 export default function Reducers(state: InitialState, action: any): InitialState {
-  // if (!action.type) return
-
-  const { currentPage } = useContext(ProductContext)
-
   const getItemByPage = (array: any[], pageNumber: number, itemsPerPage: number) => {
     const startIndex = (pageNumber - 1) * itemsPerPage
     const endIndex = startIndex + itemsPerPage
@@ -23,20 +16,93 @@ export default function Reducers(state: InitialState, action: any): InitialState
     case 'SET_PAGE_PRODUCTS':
       return {
         ...state,
-        itensPaginated: getItemByPage(state.products, state.currentPage, 6)
+        itensPaginated: getItemByPage(state.products, state.currentPage, 8)
       }
-    case 'SET_NEXT_PAGE_PRODUCTS':
+
+    case 'SET_NEW_PRODUCT':
       return {
         ...state,
-        itensPaginated: getItemByPage(state.products, state.currentPage, 10)
+        products: [...state.products, action.payload]
       }
 
 
+    case 'SET_FILTER_BY_NAME':
+      return {
+        ...state,
+        filter: {
+          ...state.filter,
+          name: action.payload
+        }
+      }
+    case 'SET_FILTER_BY_CATEGORY':
+      return {
+        ...state,
+        filter: {
+          ...state.filter,
+          category: action.payload
+        }
+      }
+
+
+    case 'SET_NEW_PRODUCT_IN_CART':
+      return {
+        ...state,
+        shoppingCart: action.payload
+      }
+
+
+    case 'SET_TOTAL_ITEMS':
+      return {
+        ...state,
+        totalPriceProducts: action.payload.reduce((total: any, product: any) => total + parseInt(product.total), 0),
+        shoppingCart: action.payload,
+      }
+
+
+
+
+    case 'SET_INCREMENT_AMOUNT':
+      return {
+        ...state,
+        shoppingCart: state.shoppingCart.map((item) => {
+          if (item.id === action.payload.id) {
+            return {
+              ...item,
+              amount: action.payload.amount,
+            }
+          }
+          return item
+        })
+      }
+
+
+
+
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        shoppingCart: state.shoppingCart.filter((cartItem) => cartItem.id !== action.payload)
+      }
+
+
+
+    case 'SET_LOADING':
+      return {
+        ...state,
+        isLoading: action.payload
+      }
+
+
+
+    case 'SET_PAGINATION':
+      return {
+        ...state,
+        itensPaginated: getItemByPage(state.products, state.currentPage, 8)
+      }
     case 'SET_NEXT_PAGE':
       return {
         ...state,
         currentPage: state.currentPage + 1,
-        itensPaginated: getItemByPage(state.products, state.currentPage, 10)
       }
     case 'SET_PREVIOUS_PAGE':
       return {
@@ -44,8 +110,8 @@ export default function Reducers(state: InitialState, action: any): InitialState
         currentPage: state.currentPage - 1,
         itensPaginated: getItemByPage(state.products, state.currentPage, 10)
       }
-
     default:
       return state
   }
+
 }
