1e9927354653307a6c7ff60bd76bfaefa7e6dfca
joão Pedro <joaof082@gmail.com>
2023-05-28 21:31:34 -0300

    feat: adding category in add product and typing response and request
    Signed-off-by: joão Pedro <joaof082@gmail.com>

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -1,6 +1,7 @@
+import { Request, Response } from "express";
 import { dbConnection } from "../../../config/db";
 
-export default async function handler(req, res) {
+export default async function handler(req: Request, res: Response) {
   switch (req.method) {
     case "GET":
       return await getProducts(req, res);
@@ -11,27 +12,28 @@ export default async function handler(req, res) {
   }
 }
 
-const getProducts = async (_, res) => {
+const getProducts = async (req: Request, res: Response) => {
   try {
     const results = await dbConnection.query("SELECT * FROM products");
-    return res.status(200).json(results);
+    return res.status(200).json(results[0]);
   } catch (error) {
     return res.status(500).json({ error });
   }
 };
 
-const saveProduct = async (req, res) => {
+const saveProduct = async (req: Request, res: Response) => {
   try {
-    const { id, name, description, price, image } = req.body;
+    const { id, name, description, price, image, category } = req.body;
 
     const result = await dbConnection.query(
-      "INSERT INTO products VALUES (:id, :name, :description, :price, :image)",
+      "INSERT INTO products VALUES (:id, :name, :description, :price, :image, :category)",
       {
         id,
         name,
         description,
         price,
         image,
+        category,
       }
     );
 
